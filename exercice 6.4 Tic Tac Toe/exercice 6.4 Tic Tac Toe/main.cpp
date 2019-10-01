727b6bc5d34633600c688d16e5c15f335df0ce21
Marvin <marvin.k.scharding@gmail.com>
2019-10-01 17:34:29 +0200

    added CheckWin and full programm

--- exercice 6.4 Tic Tac Toe/exercice 6.4 Tic Tac Toe/main.cpp
+++ exercice 6.4 Tic Tac Toe/exercice 6.4 Tic Tac Toe/main.cpp
@@ -3,9 +3,10 @@
 
 void ShowMap(std::vector<char>board);
 void GenerateMap(std::vector<char>&board);
-
 bool SetNewPositionPlayer1(std::vector<char>&board, int player1Input, int mark1);
 bool SetNewPositionPlayer2(std::vector<char>& board, int player2Input, int mark1);
+void PlayGame(std::vector<char>& board, int player1Input, int mark1, int player2Input, int mark2, bool noError1, bool noError2, bool isRunning);
+void CheckWin(std::vector<char> board, int mark1, int mark2, bool& isRunning);
 
 int main() {
 		std::vector<char>board;
@@ -14,30 +15,28 @@ int main() {
 		int player2Input = 0;
 		char mark1 = 'X';
 		char mark2 = 'O';
-		bool winner = false;
+		bool isRunning = true;
 		bool noError1 = false;
 		bool noError2 = false;
+		
 
 		GenerateMap(board);
-	ShowMap(board);
-	while (!noError1)
-	{
-		noError1 = SetNewPositionPlayer1(board, player1Input, mark1);
-	}
-	ShowMap(board);
-	while (!noError2)
-	{
-		noError2 = SetNewPositionPlayer2(board, player2Input, mark2);
-	}
-	ShowMap(board);	
-
+		PlayGame(board, player1Input, mark1, player2Input, mark2, noError1, noError2, isRunning);
+	
 
 	system("pause");
 	return EXIT_SUCCESS;
 }
+
+
+
+
+
+
+
 	void ShowMap(std::vector<char>board) {
 
-		std::cout << "  Player 1 : X  " << "  Player 2 : O \n	";
+		std::cout << " PLAYER 1 : X  " << "  PLAYER 2 : O \n	";
 		std::cout << "\n";
 
 		std::cout << "		|		|		" << "\n";
@@ -55,8 +54,8 @@ int main() {
 	bool SetNewPositionPlayer1(std::vector<char>&board, int player1Input, int mark1)
 	{
 		
-		std::cout << "Player 1, choose a new position : \n";
-		std::cin >> player1Input;
+		std::cout << "Player 1, choose a new position : \n"; std::cin >> player1Input;
+		
 
 		if (player1Input > 9 || player1Input < 1)
 		{
@@ -115,6 +114,7 @@ int main() {
 	{
 		std::cout << "Player 2, choose a new position : \n";
 		std::cin >> player2Input;
+		
 		
 		if (player2Input > 9 || player2Input < 1)
 		{
@@ -187,4 +187,170 @@ int main() {
 		
 	}
 
+	void CheckWin(std::vector<char> board, int mark1, int mark2, bool& isRunning) {
+
+		if (board[1] == board[2] && board[2] == board[3]) {
+
+			if (board[1] == mark1)
+			{
+				std::cout << "Player 1 WIN !\n";
+				isRunning = false;
+				return;
+			}
+			else
+			{
+				std::cout << "player 2 WIN !\n";
+				isRunning = false;
+				return;
+			}
+		}
+		if (board[4] == board[5] && board[5] == board[6]) {
+
+			if (board[4] == mark1)
+			{
+				std::cout << "Player 1 WIN !\n";
+				isRunning = false;
+				return;
+			}
+			else
+			{
+				std::cout << "player 2 WIN !\n";
+				isRunning = false;
+				return;
+			}
+		}
+		if (board[7] == board[8] && board[8] == board[9]) {
+
+			if (board[7] == mark1)
+			{
+				std::cout << "Player 1 WIN !\n";
+				isRunning = false;
+				return;
+			}
+			else
+			{
+				std::cout << "player 2 WIN !\n";
+				isRunning = false;
+				return;
+			}
+		}
+		if (board[1] == board[4] && board[4] == board[7]) {
+
+			if (board[1] == mark1)
+			{
+				std::cout << "Player 1 WIN !\n";
+				isRunning = false;
+				return;
+			}
+			else
+			{
+				std::cout << "player 2 WIN !\n";
+				isRunning = false;
+				return;
+			}
+		}
+		if (board[2] == board[5] && board[5] == board[8]) {
+
+			if (board[2] == mark1)
+			{
+				std::cout << "Player 1 WIN !\n";
+				isRunning = false;
+				return ;
+			}
+			else
+			{
+				std::cout << "player 2 WIN !\n";
+				isRunning = false;
+				return;
+			}
+		}
+		if (board[3] == board[6] && board[6] == board[9]) {
+
+			if (board[3] == mark1)
+			{
+				std::cout << "Player 1 WIN !\n";
+				isRunning = false;
+				return ;
+			}
+			else
+			{
+				std::cout << "player 2 WIN !\n";
+				isRunning = false;
+				return;
+			}
+		}
+		if (board[1] == board[5] && board[5] == board[9]) {
+
+			if (board[1] == mark1)
+			{
+				std::cout << "Player 1 WIN !\n";
+				isRunning = false;
+				return;
+			}
+			else
+			{
+				std::cout << "player 2 WIN !\n";
+				isRunning = false;
+				return;
+			}
+		}
+		if (board[3] == board[5] && board[5] == board[7]) {
+
+			if (board[3] == mark1)
+			{
+				std::cout << "Player 1 WIN !\n";
+				isRunning = false;
+				return;
+			}
+			else
+			{
+				std::cout << "player 2 WIN !\n";
+				isRunning = false;
+				return;
+			}
+			
+		}
+		
+	}
+
+	void PlayGame(std::vector<char>& board, int player1Input, int mark1, int player2Input, int mark2, bool noError1, bool noError2, bool isRunning)
+	{
+		int caseFull = 0;
+		while (isRunning == true)
+		{
+			ShowMap(board);
+
+			while (!noError1)
+			{
+				noError1 = SetNewPositionPlayer1(board, player1Input, mark1);
+			}
+			 CheckWin(board, mark1, mark2,  isRunning);
+			 if (!isRunning) {
+				 return;
+			 }
+			 caseFull++;
+			 if (caseFull == 9)
+			 {
+				 std::cout << "IT'S A TIE !\n";
+				 break;
+			 }
+			ShowMap(board);
+			while (!noError2)
+			{
+				noError2 = SetNewPositionPlayer2(board, player2Input, mark2);
+			}
+			CheckWin(board, mark1, mark2, isRunning);
+			if (!isRunning) {
+				return;
+			}
+			caseFull++;
+			if (caseFull == 9)
+			{
+				std::cout << "It's a tie\n";
+				break;
+			}
+			noError1 = false;
+			noError2 = false;
+		}
+	}
 	
