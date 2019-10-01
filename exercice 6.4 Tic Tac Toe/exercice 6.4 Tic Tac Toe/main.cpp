4cbecf6b3e613786ba6661151fa1c7e7a2165f0d
Marvin <marvin.k.scharding@gmail.com>
2019-10-01 14:54:51 +0200

    added some security test

--- exercice 6.4 Tic Tac Toe/exercice 6.4 Tic Tac Toe/main.cpp
+++ exercice 6.4 Tic Tac Toe/exercice 6.4 Tic Tac Toe/main.cpp
@@ -3,9 +3,9 @@
 
 void ShowMap(std::vector<char>board);
 void GenerateMap(std::vector<char>&board);
-void ChosePlayerMark(int& mark1, int& mark2);
-void SetNewPositionPlayer1(std::vector<char>&board, int player1Input, int mark1);
-void SetNewPositionPlayer2(std::vector<char>& board, int player2Input, int mark1);
+
+bool SetNewPositionPlayer1(std::vector<char>&board, int player1Input, int mark1);
+bool SetNewPositionPlayer2(std::vector<char>& board, int player2Input, int mark1);
 
 int main() {
 		std::vector<char>board;
@@ -14,14 +14,22 @@ int main() {
 		int player2Input = 0;
 		char mark1 = 'X';
 		char mark2 = 'O';
+		bool winner = false;
+		bool noError1 = false;
+		bool noError2 = false;
+
 		GenerateMap(board);
 	ShowMap(board);
-	/*ChosePlayerMark(mark1, mark2);*/
-	SetNewPositionPlayer1(board, player1Input, mark1);
+	while (!noError1)
+	{
+		noError1 = SetNewPositionPlayer1(board, player1Input, mark1);
+	}
 	ShowMap(board);
-	SetNewPositionPlayer2(board, player2Input, mark2);
-	ShowMap(board);
-
+	while (!noError2)
+	{
+		noError2 = SetNewPositionPlayer2(board, player2Input, mark2);
+	}
+	ShowMap(board);	
 
 
 	system("pause");
@@ -29,6 +37,8 @@ int main() {
 }
 	void ShowMap(std::vector<char>board) {
 
+		std::cout << "  Player 1 : X  " << "  Player 2 : O \n	";
+		std::cout << "\n";
 
 		std::cout << "		|		|		" << "\n";
 		std::cout << "	" << board[1] << "	|	" << board[2] << "	|	" << board[3] << "\n";
@@ -42,10 +52,26 @@ int main() {
 		std::cout << "\n";
 	}
 
-	void SetNewPositionPlayer1(std::vector<char>&board, int player1Input, int mark1)
+	bool SetNewPositionPlayer1(std::vector<char>&board, int player1Input, int mark1)
 	{
+		
 		std::cout << "Player 1, choose a new position : \n";
 		std::cin >> player1Input;
+
+		if (player1Input > 9 || player1Input < 1)
+		{
+			std::cout << "Impossible \n ";
+			std::cout << "\n";
+			return false;
+		}
+
+		if (board[player1Input] == 'O'|| board[player1Input] == 'X')
+		{
+			std::cout << "Impossible \n ";
+			std::cout << "\n";
+			return false;
+		}
+		
 
 		if (player1Input == 1 && board[1] == '1') 
 		{
@@ -85,47 +111,63 @@ int main() {
 		}
 	}
 
-	void SetNewPositionPlayer2(std::vector<char>& board, int player2Input, int mark2)
+	bool SetNewPositionPlayer2(std::vector<char>& board, int player2Input, int mark2)
 	{
 		std::cout << "Player 2, choose a new position : \n";
 		std::cin >> player2Input;
+		
+		if (player2Input > 9 || player2Input < 1)
+		{
+			std::cout << "Impossible \n ";
+			std::cout << "\n";
+			return false;
+			
+		}
 
-		if (player2Input == 1 && board[1] == '1')
+		if (board[player2Input] == 'O'|| board[player2Input] == 'X')
 		{
-			board[1] = mark2;
+			std::cout << "Impossible \n";
+			std::cout << "\n";
+			
+			return false;
 		}
-		if (player2Input == 2 && board[2] == '2')
-		{
-			board[2] = mark2;
-		}
-		if (player2Input == 3 && board[3] == '3')
-		{
-			board[3] = mark2;
-		}
-		if (player2Input == 4 && board[4] == '4')
-		{
-			board[4] = mark2;
-		}
-		if (player2Input == 5 && board[5] == '5')
-		{
-			board[5] = mark2;
-		}
-		if (player2Input == 6 && board[6] == '6')
-		{
-			board[6] = mark2;
-		}
-		if (player2Input == 7 && board[7] == '7')
-		{
-			board[7] = mark2;
-		}
-		if (player2Input == 8 && board[8] == '8')
-		{
-			board[8] = mark2;
-		}
-		if (player2Input == 9 && board[9] == '9')
-		{
-			board[9] = mark2;
-		}
+			if (player2Input == 1 && board[1] == '1')
+			{
+				board[1] = mark2;
+			}
+			if (player2Input == 2 && board[2] == '2')
+			{
+				board[2] = mark2;
+			}
+			if (player2Input == 3 && board[3] == '3')
+			{
+				board[3] = mark2;
+			}
+			if (player2Input == 4 && board[4] == '4')
+			{
+				board[4] = mark2;
+			}
+			if (player2Input == 5 && board[5] == '5')
+			{
+				board[5] = mark2;
+			}
+			if (player2Input == 6 && board[6] == '6')
+			{
+				board[6] = mark2;
+			}
+			if (player2Input == 7 && board[7] == '7')
+			{
+				board[7] = mark2;
+			}
+			if (player2Input == 8 && board[8] == '8')
+			{
+				board[8] = mark2;
+			}
+			if (player2Input == 9 && board[9] == '9')
+			{
+				board[9] = mark2;
+			}
+		
 	}
 
 	void GenerateMap(std::vector<char>&board)
@@ -145,23 +187,4 @@ int main() {
 		
 	}
 
-	void ChosePlayerMark(int& mark1, int& mark2) {
-
-		int player1mark = 0;
-
-		player1mark = rand() % 2 + 1;
-
-		switch (player1mark)
-		{
-		case (1):
-
-			mark1 = 'O';
-			mark2 = 'X';
-			break;
-		case (2):
-
-			mark1 = 'X';
-			mark2 = 'O';
-			break;
-		}
-	}
+	
